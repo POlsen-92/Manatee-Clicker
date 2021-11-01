22ac83cead669d136aaa1642fd8a9e23b6321352
Jorge Barragan <jmarq19@outlook.com>
2021-11-01 14:02:54 -0700

    debugging loging first wave

--- routes/frontend-routes.js
+++ routes/frontend-routes.js
@@ -6,13 +6,8 @@ router.get("/dashboard", (req,res)=>{
     res.render("dashboard")
 })
 
-<<<<<<< HEAD
-router.get("/login", (req,res)=>{
-    res.render("login")
-=======
 router.get('/login', (req,res)=>{
     res.render('login')
->>>>>>> 1a723574101b26a621f531beb081d3e4011d5d43
 });
 
 module.exports = router;
