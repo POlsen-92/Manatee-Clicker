686ea4429c389537e9be85d0191c2037169b7988
FemBot 9000 <paige.el.olsen@gmail.com>
2021-11-05 13:14:54 -0700

    adding autoclicker to unicorn manatee

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -11,47 +11,46 @@ let accountantLevel
 let policemanateeLevel
 let unicornLevel
 let lawyerLevel
-let clickValueOutside
 
 // GRABS ALL API INFO, AND POPULATES THE PAGE WITH THE INFO
 const onLoad = () => {
     fetch("/api/users/info")
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            const userId = data.id 
-            tempId = userId
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        const userId = data.id
+        tempId = userId
 
-            accountantLevel = data.manatees[0].user_manatee.count
-            policemanateeLevel = data.manatees[1].user_manatee.count
-            lawyerLevel = data.manatees[2].user_manatee.count
-            unicornLevel = data.manatees[3].user_manatee.count
-            clickValue = accountantLevel + (policemanateeLevel * 10) + (unicornLevel * 100)+ (lawyerLevel * 1000)
+        accountantLevel = data.manatees[0].user_manatee.count
+        policemanateeLevel = data.manatees[1].user_manatee.count
+        lawyerLevel = data.manatees[2].user_manatee.count
+        unicornLevel = data.manatees[3].user_manatee.count
+        clickValue = accountantLevel + (policemanateeLevel * 10) + (unicornLevel * 100) + (lawyerLevel * 1000)
+        console.log(clickValue)
 
-            manateeLevel.innerHTML = clickValue
-            const lifetimePoints = data.lifetime_points
-            lifetimePointsText.innerHTML = lifetimePoints
-            pointsOnHand = data.points_on_hand
-            pointsOnHandText.value = pointsOnHand
-            
-            costAccountant.innerHTML = (accountantLevel+1) * 10;
-            costPolicemanatee.innerHTML = (policemanateeLevel+1) *100;
-            costLawyer.innerHTML = (lawyerLevel+1) *1000;
-            costUnicorn.innerHTML = (unicornLevel+1) *10000;
-
-        })
-    }
+        manateeLevel.innerHTML = clickValue
+        const lifetimePoints = data.lifetime_points
+        lifetimePointsText.innerHTML = lifetimePoints
+        pointsOnHand = data.points_on_hand
+        pointsOnHandText.value = pointsOnHand
+        
+        costAccountant.innerHTML = (accountantLevel + 1) * 10;
+        costPolicemanatee.innerHTML = (policemanateeLevel + 1) *100;
+        costLawyer.innerHTML = (lawyerLevel + 1) *1000;
+        costUnicorn.innerHTML = (unicornLevel + 1) *10000;
+    })
+}
 
 // UPDATE() FUNCTION THAT SAVES THE STATS ON SCREEN TO THE CURRENT NUMBERS
-const update = () =>{
-        fetch("/api/users/updatepoints", {
+const update = () => {
+    fetch("/api/users/updatepoints", {
         method: "PUT",
         body: JSON.stringify({
             points_on_hand: pointsOnHandText.value,
             lifetime_points:lifetimePointsText.innerHTML
         }),
-        headers:{"Content-Type":"application/json"}
+        headers:{ "Content-Type":"application/json"  }
     })
     .then(response=>{
         onLoad();
@@ -60,31 +59,38 @@ const update = () =>{
 
 // MANATEE BUTTON THAT ADDS TO SCORE
 document.getElementById("click-button").addEventListener("click", ()=>{
-    const power = Number(manateeLevel.innerHTML)+1
+    const power = Number(manateeLevel.innerHTML) - (unicornLevel * 100) + 1
+    console.log(power)
     lifetimePointsText.innerHTML = Number(lifetimePointsText.innerHTML) + (power)
     pointsOnHandText.value = Number(pointsOnHandText.value) + (power)
     update()
 });
 
+// FUNCTION THAT TURNS UNICORN LEVEL INTO AUTOCLICKER
+const autoclick = () => {
+    const clickpersec = unicornLevel
+    
+}
+
+
 // FUNCTION THAT HANDLES THE PURCHASE OF NEW MANATEES
 buyButton.forEach((el)=>{el.addEventListener("click", (event) => {
     event.preventDefault()
-    const id = el.id    
+    const id = el.id
     let cost = Number(el.parentElement.lastElementChild.innerHTML)
 
     if (Number(pointsOnHandText.value) >= cost) {
-            fetch(`/api/usermanatees`, {
-                method: "PUT",
-                body: JSON.stringify({
-                    manatee_id:id,
-                }),
-                headers:{"Content-Type":"application/json"}
-            })
-            pointsOnHandText.value= pointsOnHandText.value - cost
-            update()
-        }
+        fetch(`/api/usermanatees`, {
+            method: "PUT",
+            body: JSON.stringify({
+                manatee_id:id,
+            }),
+            headers:{"Content-Type":"application/json"}
+        })
+        pointsOnHandText.value= pointsOnHandText.value - cost
+        update()
     }
-)})
+})})
 
 
 onLoad()
