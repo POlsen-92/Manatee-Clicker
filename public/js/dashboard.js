a5ad41cd1b075a9ade4cb85194fd898cabae8618
FemBot 9000 <paige.el.olsen@gmail.com>
2021-11-05 11:51:40 -0700

    fixed async buy, removed some console.logs

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -20,7 +20,6 @@ const onLoad = () => {
             return response.json();
         })
         .then(data => {
-            console.log(data)
             const userId = data.id 
             tempId = userId
 
@@ -45,8 +44,7 @@ const onLoad = () => {
     }
 
 // UPDATE() FUNCTION THAT SAVES THE STATS ON SCREEN TO THE CURRENT NUMBERS
-const update = ()=>{
-    // console.log()
+const update = () =>{
         fetch("/api/users/updatepoints", {
         method: "PUT",
         body: JSON.stringify({
@@ -55,13 +53,14 @@ const update = ()=>{
         }),
         headers:{"Content-Type":"application/json"}
     })
-    .then(response=>response.json())
+    .then(response=>{
+        onLoad();
+    })
 }
 
 // MANATEE BUTTON THAT ADDS TO SCORE
 document.getElementById("click-button").addEventListener("click", ()=>{
     const power = Number(manateeLevel.innerHTML)+1
-    console.log(`clickpower:${power}`)
     lifetimePointsText.innerHTML = Number(lifetimePointsText.innerHTML) + (power)
     pointsOnHandText.value = Number(pointsOnHandText.value) + (power)
     update()
@@ -73,10 +72,7 @@ buyButton.forEach((el)=>{el.addEventListener("click", (event) => {
     const id = el.id    
     let cost = Number(el.parentElement.lastElementChild.innerHTML)
 
-    console.log("Cost = "+cost)
-    console.log("Points on Hand = " +pointsOnHandText.value)
     if (Number(pointsOnHandText.value) >= cost) {
-        console.log("========STARTING FETCH============")
             fetch(`/api/usermanatees`, {
                 method: "PUT",
                 body: JSON.stringify({
@@ -84,17 +80,8 @@ buyButton.forEach((el)=>{el.addEventListener("click", (event) => {
                 }),
                 headers:{"Content-Type":"application/json"}
             })
-            console.log("=============FETCH COMPLETE==========")
             pointsOnHandText.value= pointsOnHandText.value - cost
-            console.log(`cost:${cost}`)
-            const upload = async () => {
-                let up = await update(1,1);
-                let on = await onLoad(2,2);
-
-                let final = up + on;
-                return final;
-            }
-            upload();
+            update()
         }
     }
 )})
